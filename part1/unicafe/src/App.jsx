b50c6116eab4b24dbd684e064b21b5f567a9227e
José Bezzeris Contreras G <josebezzeris@gmail.com>
2025-05-15 12:59:47 -0600

    Part1: 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
+// Definición de componente
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good: {props.good}</p>
+      <p>Neutral: {props.neutral}</p>
+      <p>Bad: {props.bad}</p>
+      <p>All: {props.all}</p>
+      <p>Average: {props.average}</p>
+      <p>Positive: {props.positive} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // Clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
@@ -26,9 +41,25 @@ const App = () => {
     setAll(all + 1)
   }
 
-  const average = (good - bad) / all
+  // Función average + 0 en vez de NaN
+  const average = () => {
+    if (all === 0){
+      return 0
+    } else {
+      return (good - bad) / all
+    }
+  }
+  console.log("Average: ", average())
 
-  const positive = (good / all) * 100
+  // Función positive + 0 en vez de NaN
+  const positive = () => {
+    if (all === 0){
+      return 0
+    } else {
+      return (good / all) * 100
+    }
+  }
+  console.log("Positive: ", positive())
   
   return (
     <div>
@@ -36,13 +67,7 @@ const App = () => {
       <button onClick={handleGood}>Good</button>
       <button onClick={handleNeutral}>Neutral</button>
       <button onClick={handleBad}>Bad</button>
-      <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {all}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average()} positive={positive()} />
     </div>
   )
 }
